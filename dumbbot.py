d9da61573924785f8b63502fc97b7f5e39df55bd
btomiuk <btomiuk@umich.edu>
2017-11-26 15:44:30 -0500

    added /pokedex, but checking if the input pokemon is in the pokedex doesn't work

--- dumbbot.py
+++ dumbbot.py
@@ -27,7 +27,7 @@ def on_message(message):
 		if not message.attachments:
 			yield from bot.delete_message(message)
 	elif message.content == 'kk':
-		yield from bot.send_message(message.channel, 'It\'s kkk ya fag')
+		yield from bot.send_message(message.channel, 'kkk*')
 
 	yield from bot.process_commands(message)
 
@@ -47,14 +47,23 @@ def brian():
 
 @bot.command()
 @asyncio.coroutine
-def link(arg):
+def ryan():
+	'''Just tells it as it is
+	Ryan is such a faggot, ugh, hate that guy'''
+	yield from bot.say('Ryan is a faggot')
+
+@bot.command()
+@asyncio.coroutine
+def link(*arg):
 	'''Provides the link desired
 	<arg> which link you want. Links are:
 	  api
 	  rl
 	  ?
 	  repo'''
-	if arg == 'api':
+	if len(arg) != 2:
+		yield from bot.say('use \'/help link\'')
+	elif arg == 'api':
 		yield from bot.say('https://discordpy.readthedocs.io/en/latest/api.html')
 	elif arg == 'rl':
 		yield from bot.say('https://www.twitch.tv/rocketleague')
@@ -70,7 +79,83 @@ def link(arg):
 def bet(*args):
 	'''YOU WANNA BET??
 	<argn> nth bet string'''
-	yield from bot.say(random.choice(args))
+	if len(args) > 1:
+		yield from bot.say(random.choice(args))
+	else:
+		yield from bot.say('use \'/help bet\'')
+
+@bot.command()
+@asyncio.coroutine
+def pokedex(*arg):
+	'''Shows which Pokemon the user still needs to catch
+	<arg1>:
+	  brian: shows Brians needed pokemon
+	  ty: shows Tys needed pokemon
+	  example: /pokedex brian
+	<arg1> <arg2>: adds a pokemon to the pokedex
+	  <arg1> same as previous
+	  <arg2> name of pokemon to be added
+	  example: /pokedex brian Bulbasaur
+	  	Yes, the capitalization is required'''
+	if len(arg) == 1:
+		dex = open("pokedex.txt","r")
+
+		if arg[0]=='brian':
+			file = open("brian_dex.txt","r")
+			need = open("brian_need.txt","w")
+		elif arg[0]=='ty':
+			file = open("ty_dex.txt","r")
+			need = open("ty_need.txt","w")
+
+		cont = file.readlines()
+		
+		for line in dex:
+			if not line in cont:
+				need.write(line)
+		need.close()
+
+		if arg[0]=='brian':
+			need = open("brian_need.txt","r")
+		elif arg[0]=='ty':
+			need = open("ty_need.txt","r")
+
+		yield from bot.say('{} still needs:'.format(arg[0]))
+		yield from bot.say(need.read())
+
+		file.close()
+		dex.close()
+		need.close()
+	elif len(arg) == 2:
+		dex = open("pokedex.txt","r")
+		complete = dex.readlines()
+		dex.close()
+
+		if arg[1] in complete:
+			if arg[0]=='brian':
+				file = open("brian_dex.txt","r")
+			elif arg[0]=='ty':
+				file = open("ty_dex.txt","r")
+
+			cont = file.readlines()
+			file.close()
+
+			if arg[1] in cont:
+				if arg[0]=='brian':
+					file = open("brian_dex.txt","w")
+				elif arg[0]=='ty':
+					file = open("ty_dex.txt","w")
+
+				for line in cont:
+					file.write(line)
+				file.write(arg[1])
+				file.write('\n')
+				file.close()
+			else:
+				yield from bot.say('This Pokemon is already registered in {}s Pokedex'.format(arg[0]))
+		else:
+			yield from bot.say('Invalid Pokemon, be sure you capitilized correctly')
+	else:
+		yield from bot.say('use \'/help pokedex\'')
 
 @bot.command(pass_context=True)
 @asyncio.coroutine
